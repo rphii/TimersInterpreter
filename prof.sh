7076dd1cab8df6bf4b480b6ded6d83d40f081328
rphii <raphael.seitz18@gmail.com>
2023-08-28 03:17:50 +0200

    fix

--- prof.sh
+++ prof.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-$execute=timers
-cmd="\"./$execute\" && gprof \"$execute\" gmon.out > \"prof_$execute.txt\" && rm gmon.out"
+execute="timers"
+cmd="./$execute $@ && gprof ./$execute gmon.out > prof_$execute.txt && rm gmon.out"
 echo $cmd
 eval $cmd
+
